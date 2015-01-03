12bea29074fd67090664cb800bd0cae336782e9b
Hasham Malik <hasham2@gmail.com>
2015-01-03 13:17:03 +0500

    updated feature to use test database

--- spec/features/search_spec.rb
+++ spec/features/search_spec.rb
@@ -1,6 +1,13 @@
 require 'spec_helper'
 
 feature "Looking up the recipes", js: true do
+  
+  before do
+    Recipe.create!(name: 'Baked Potato w/ Cheese')
+    Recipe.create!(name: 'Fried Salmon')
+    Recipe.create!(name: 'Pepperoni Pizza')
+  end
+
   scenario "finding recipes" do
     visit '/'
     fill_in "keywords", with: "baked"
